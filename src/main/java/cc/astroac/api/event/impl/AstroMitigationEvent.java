c5485f6f11017cb705fa6c14d7e7e6b04c62aaac
SomeRandomDev <41921648+ChangDaddy@users.noreply.github.com>
2023-08-22 21:03:56 -0400

    Extend AstroEvent for mitigation event class

--- src/main/java/cc/astroac/api/event/impl/AstroMitigationEvent.java
+++ src/main/java/cc/astroac/api/event/impl/AstroMitigationEvent.java
@@ -1,12 +1,13 @@
 package cc.astroac.api.event.impl;
 
+import cc.astroac.api.event.AstroEvent;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.bukkit.entity.Player;
 
 @Getter
 @AllArgsConstructor
-public class AstroMitigationEvent {
+public class AstroMitigationEvent extends AstroEvent {
     private final Player player;
     private final String check, type, info, description;
     private final int currentViolationLevel, maxVl;
