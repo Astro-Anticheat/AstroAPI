91f5ae1f7644a0c71caded2366a821801d00d4a7
FinalBoolean <WizardPros124@gmail.com>
2023-11-30 00:19:44 -0500

    Don't use @Data...

--- src/main/java/cc/astroac/api/event/impl/AstroAddUserEvent.java
+++ src/main/java/cc/astroac/api/event/impl/AstroAddUserEvent.java
@@ -3,12 +3,15 @@ package cc.astroac.api.event.impl;
 import cc.astroac.api.event.AstroEvent;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.UUID;
 
-@Data
+@Getter
 @AllArgsConstructor
 public class AstroAddUserEvent extends AstroEvent {
+    @Setter
     private int protocolVersion;
     private UUID uuid;
 }
